d0cec23c9a4d9156f85d76b9b2bf07626392bd30
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-01-23 15:27:27 +0100

    Lavoro Vue Hello

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            title: 'Hello Vue!',
+            img: "https://picsum.photos/200",
+            
+        }
+    },
+
+}).mount('#app');
